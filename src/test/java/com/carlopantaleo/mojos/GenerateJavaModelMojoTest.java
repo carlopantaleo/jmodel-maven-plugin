3920c9a9a6a79fa0db421208df382e45a2edfc60
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2018-03-06 17:23:09 +0100

    Crafting first unit test.
    More to come...

--- src/test/java/com/carlopantaleo/mojos/GenerateJavaModelMojoTest.java
+++ src/test/java/com/carlopantaleo/mojos/GenerateJavaModelMojoTest.java
@@ -1,11 +1,32 @@
 package com.carlopantaleo.mojos;
 
 
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class GenerateJavaModelMojoTest {
+    private final String generatedDir = System.getProperty("user.dir") + "/src/main/java/com/jmodel/generated/";
+
     @Test
     public void works() throws Exception {
+        execute();
+
+        Path path = new File(generatedDir + "TestTable.java").toPath();
+        List<String> lines = Files.readAllLines(path, Charset.forName("UTF-8"));
+        assertEquals(79, lines.size());
+    }
+
+    private void execute() throws MojoExecutionException, MojoFailureException {
         GenerateJavaModelMojo generateJavaModelMojo = new GenerateJavaModelMojo();
         generateJavaModelMojo.setJmodelFileName("jmodel.xml");
         generateJavaModelMojo.setRootPackageDir(System.getProperty("user.dir"));

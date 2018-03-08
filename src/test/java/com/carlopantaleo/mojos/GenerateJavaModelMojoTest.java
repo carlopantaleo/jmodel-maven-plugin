8a655e05990251813704e28edab0641a1b6983ad
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2018-03-08 17:03:16 +0100

    More unit tests

--- src/test/java/com/carlopantaleo/mojos/GenerateJavaModelMojoTest.java
+++ src/test/java/com/carlopantaleo/mojos/GenerateJavaModelMojoTest.java
@@ -3,7 +3,6 @@ package com.carlopantaleo.mojos;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
@@ -18,12 +17,50 @@ public class GenerateJavaModelMojoTest {
     private final String generatedDir = System.getProperty("user.dir") + "/src/main/java/com/jmodel/generated/";
 
     @Test
-    public void works() throws Exception {
+    public void testTableIsGeneratedCorrectly() throws Exception {
         execute();
 
         Path path = new File(generatedDir + "TestTable.java").toPath();
         List<String> lines = Files.readAllLines(path, Charset.forName("UTF-8"));
         assertEquals(79, lines.size());
+        assertEquals("private String primaryKey;", lines.get(6).trim());
+        assertEquals("private @Nullable String secondField;", lines.get(7).trim());
+        assertEquals("private String thirdField;", lines.get(8).trim());
+        assertEquals("private BigDecimal numberField;", lines.get(9).trim());
+        assertEquals("private double doubleField;", lines.get(10).trim());
+        assertEquals("private float floatField;", lines.get(11).trim());
+        assertEquals("private TestEnum enumField;", lines.get(12).trim());
+        assertEquals("private boolean booleanField;", lines.get(13).trim());
+
+        assertEquals("public String getPrimaryKey() {", lines.get(15).trim());
+        assertEquals("public void setPrimaryKey(String primaryKey) {", lines.get(19).trim());
+        assertEquals("public @Nullable String getSecondField() {", lines.get(23).trim());
+        assertEquals("public void setSecondField(@Nullable String secondField) {", lines.get(27).trim());
+        assertEquals("public String getThirdField() {", lines.get(31).trim());
+        assertEquals("public void setThirdField(String thirdField) {", lines.get(35).trim());
+        assertEquals("public BigDecimal getNumberField() {", lines.get(39).trim());
+        assertEquals("public void setNumberField(BigDecimal numberField) {", lines.get(43).trim());
+        assertEquals("public double getDoubleField() {", lines.get(47).trim());
+        assertEquals("public void setDoubleField(double doubleField) {", lines.get(51).trim());
+        assertEquals("public float getFloatField() {", lines.get(55).trim());
+        assertEquals("public void setFloatField(float floatField) {", lines.get(59).trim());
+        assertEquals("public TestEnum getEnumField() {", lines.get(63).trim());
+        assertEquals("public void setEnumField(TestEnum enumField) {", lines.get(67).trim());
+        assertEquals("public boolean getBooleanField() {", lines.get(71).trim());
+        assertEquals("public void setBooleanField(boolean booleanField) {", lines.get(75).trim());
+    }
+
+    @Test
+    public void testEnumIsGeneratedCorrectly() throws Exception {
+        execute();
+
+        Path path = new File(generatedDir + "TestEnum.java").toPath();
+        List<String> lines = Files.readAllLines(path, Charset.forName("UTF-8"));
+
+        assertEquals(6, lines.size());
+        assertEquals("public enum TestEnum {", lines.get(2).trim());
+        assertEquals("ITEM1,", lines.get(3).trim());
+        assertEquals("ITEM2", lines.get(4).trim());
     }
 
     private void execute() throws MojoExecutionException, MojoFailureException {

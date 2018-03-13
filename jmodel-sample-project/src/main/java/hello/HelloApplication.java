c9c922bcefb254e1299ec966a3ce397e96a0ac5e
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2018-03-13 18:10:35 +0100

    Refactored the whole project structure to include 2 modules: the main JModel plugin and a sample JModel project

---
+++ jmodel-sample-project/src/main/java/hello/HelloApplication.java
@@ -0,0 +1,33 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class HelloApplication {
+    private static final Logger log = LoggerFactory.getLogger(HelloApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            log.info("Application running under Spring Boot with the following dependencies:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                log.info(beanName);
+            }
+        };
+    }
+}

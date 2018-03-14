94529487614f1b92697e78101e302faf31215f8a
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2018-03-14 16:55:55 +0100

    Entities are extracted in a utility class and not in JavaModelGenerator
    More than one generator will use entities extracted from xml, so it's worth to have a common utility class.

--- jmodel-maven-plugin/src/main/java/com/carlopantaleo/generators/JavaModelGenerator.java
+++ jmodel-maven-plugin/src/main/java/com/carlopantaleo/generators/JavaModelGenerator.java
@@ -4,6 +4,7 @@ import com.carlopantaleo.entities.Enum;
 import com.carlopantaleo.entities.Field;
 import com.carlopantaleo.entities.Table;
 import com.carlopantaleo.exceptions.ValidationException;
+import com.carlopantaleo.utils.EntitesExtractor;
 import com.carlopantaleo.utils.SnakeCaseToCamelcase;
 import com.google.common.collect.Sets;
 import com.google.googlejavaformat.java.Formatter;
@@ -47,151 +48,14 @@ public class JavaModelGenerator {
     public void generateSources() throws Exception {
         validateInput();
 
-        for (Table table : extractTablesAndFields()) {
+        EntitesExtractor extractor = new EntitesExtractor(model);
+
+        for (Table table : extractor.extractTablesAndFields()) {
             writeTableJavaFile(table);
         }
 
-        for (Enum theEnum : extractEnumsAndItems()) {
+        for (Enum theEnum : extractor.extractEnumsAndItems()) {
             writeEnumJavaFile(theEnum);
-        }
-    }
-
-    private List<Table> extractTablesAndFields() throws XPathExpressionException, ValidationException {
-        List<Table> tables = new ArrayList<>();
-
-        extractTables(tables);
-        extractFields(tables);
-        return tables;
-    }
-
-    private List<Enum> extractEnumsAndItems() throws XPathExpressionException, ValidationException {
-        List<Enum> enums = new ArrayList<>();
-
-        extractEnums(enums);
-        extractItems(enums);
-        return enums;
-    }
-
-    private void extractTables(List<Table> tables) throws XPathExpressionException {
-        NodeList tablesNodes = (NodeList) xPath
-                .compile("jmodel/table")
-                .evaluate(model, XPathConstants.NODESET);
-
-        for (int i = 0; i < tablesNodes.getLength(); i++) {
-            Table table = new Table();
-            table.setName(tablesNodes.item(i)
-                    .getAttributes()
-                    .getNamedItem("name")
-                    .getNodeValue());
-            tables.add(table);
-        }
-    }
-
-    private void extractFields(List<Table> tables) throws XPathExpressionException, ValidationException {
-        for (Table table : tables) {
-            NodeList fields = (NodeList) xPath
-                    .compile(String.format("jmodel/table[@name='%s']/fields/field", table.getName()))
-                    .evaluate(model, XPathConstants.NODESET);
-
-            for (int i = 0; i < fields.getLength(); i++) {
-                Field field = makeField(fields.item(i));
-                table.addField(field);
-                if (field.isPk()) {
-                    table.addPkField(field);
-                }
-            }
-        }
-    }
-
-    private void extractEnums(List<Enum> enums) throws XPathExpressionException {
-        NodeList tablesNodes = (NodeList) xPath
-                .compile("jmodel/enum")
-                .evaluate(model, XPathConstants.NODESET);
-
-        for (int i = 0; i < tablesNodes.getLength(); i++) {
-            Enum theEnum = new Enum();
-            theEnum.setName(tablesNodes.item(i)
-                    .getAttributes()
-                    .getNamedItem("name")
-                    .getNodeValue());
-            enums.add(theEnum);
-        }
-    }
-
-    private void extractItems(List<Enum> enums) throws XPathExpressionException, ValidationException {
-        for (Enum theEnum : enums) {
-            NodeList items = (NodeList) xPath
-                    .compile(String.format("jmodel/enum[@name='%s']/item", theEnum.getName()))
-                    .evaluate(model, XPathConstants.NODESET);
-
-            for (int i = 0; i < items.getLength(); i++) {
-                String item = null;
-                if (items.item(i).getFirstChild().getNodeValue() != null) {
-                    item = items.item(i).getFirstChild().getNodeValue().trim();
-                }
-                if (item == null || item.isEmpty()) {
-                    throw new ValidationException("Enum " + theEnum.getName() + " cannot have empty items");
-                }
-
-                theEnum.addItem(item);
-            }
-        }
-    }
-
-
-    private Field makeField(Node item) throws ValidationException {
-        NamedNodeMap attributes = item.getAttributes();
-        String name = item.getFirstChild().getNodeValue() != null ?
-                item.getFirstChild().getNodeValue().trim() : null;
-        String type = attributes.getNamedItem("type") != null ?
-                attributes.getNamedItem("type").getNodeValue() : null;
-        int length = attributes.getNamedItem("length") != null ?
-                Integer.parseInt(attributes.getNamedItem("length").getNodeValue()) : 0;
-        boolean pk = attributes.getNamedItem("pk") != null &&
-                Boolean.parseBoolean(attributes.getNamedItem("pk").getNodeValue());
-        boolean nullable = attributes.getNamedItem("nullable") != null &&
-                Boolean.parseBoolean(attributes.getNamedItem("nullable").getNodeValue());
-        String defaultVal = attributes.getNamedItem("default") != null ?
-                attributes.getNamedItem("default").getNodeValue() : null;
-        String referredEnum = attributes.getNamedItem("referred-enum") != null ?
-                attributes.getNamedItem("referred-enum").getNodeValue() : null;
-
-        if (type == null) {
-            throw new ValidationException("Attribute 'type' for field '" + name + "' is missing.");
-        }
-
-        Field field = new Field();
-        field.setName(name);
-        field.setType(parseFieldType(type));
-        if (field.getType() == Enum.class) {
-            field.setReferredEnum(referredEnum);
-        }
-        field.setDefaultVal(defaultVal);
-        field.setNullable(nullable);
-        field.setLenght(length);
-        field.setPk(pk);
-
-        return field;
-    }
-
-    private Class parseFieldType(String type) {
-        switch (type) {
-            case "string":
-                return String.class;
-            case "number":
-                return BigDecimal.class;
-            case "long":
-                return Long.class;
-            case "double":
-                return Double.class;
-            case "float":
-                return Float.class;
-            case "boolean":
-                return Boolean.class;
-            case "enum":
-                return Enum.class;
-            default:
-                return null;
         }
     }
 
@@ -359,7 +223,7 @@ public class JavaModelGenerator {
         if (destinationPackage == null) {
             throw new ValidationException("destinationPackage cannot be null.");
         }
-        String pattern = "^(?!\\.)[a-z\\.]*[a-z]$";
+        String pattern = "^(?!\\.)[a-z.]*[a-z]$";
         if (!destinationPackage.matches(pattern)) {
             throw new ValidationException(destinationPackage, pattern);
         }
---
+++ jmodel-maven-plugin/src/main/java/com/carlopantaleo/utils/EntitesExtractor.java
@@ -0,0 +1,165 @@
+package com.carlopantaleo.utils;
+
+import com.carlopantaleo.entities.Enum;
+import com.carlopantaleo.entities.Field;
+import com.carlopantaleo.entities.Table;
+import com.carlopantaleo.exceptions.ValidationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntitesExtractor {
+    private final Document model;
+    private final XPath xPath = XPathFactory.newInstance().newXPath();
+
+    public EntitesExtractor(Document model) {
+        this.model = model;
+    }
+
+    public List<Table> extractTablesAndFields() throws XPathExpressionException, ValidationException {
+        List<Table> tables = new ArrayList<>();
+
+        extractTables(tables);
+        extractFields(tables);
+        return tables;
+    }
+
+    private void extractTables(List<Table> tables) throws XPathExpressionException {
+        NodeList tablesNodes = (NodeList) xPath
+                .compile("jmodel/table")
+                .evaluate(model, XPathConstants.NODESET);
+
+        for (int i = 0; i < tablesNodes.getLength(); i++) {
+            Table table = new Table();
+            table.setName(tablesNodes.item(i)
+                    .getAttributes()
+                    .getNamedItem("name")
+                    .getNodeValue());
+            tables.add(table);
+        }
+    }
+
+    private void extractFields(List<Table> tables) throws XPathExpressionException, ValidationException {
+        for (Table table : tables) {
+            NodeList fields = (NodeList) xPath
+                    .compile(String.format("jmodel/table[@name='%s']/fields/field", table.getName()))
+                    .evaluate(model, XPathConstants.NODESET);
+
+            for (int i = 0; i < fields.getLength(); i++) {
+                Field field = makeField(fields.item(i));
+                table.addField(field);
+                if (field.isPk()) {
+                    table.addPkField(field);
+                }
+            }
+        }
+    }
+
+    private Field makeField(Node item) throws ValidationException {
+        NamedNodeMap attributes = item.getAttributes();
+        String name = item.getFirstChild().getNodeValue() != null ?
+                item.getFirstChild().getNodeValue().trim() : null;
+        String type = attributes.getNamedItem("type") != null ?
+                attributes.getNamedItem("type").getNodeValue() : null;
+        int length = attributes.getNamedItem("length") != null ?
+                Integer.parseInt(attributes.getNamedItem("length").getNodeValue()) : 0;
+        boolean pk = attributes.getNamedItem("pk") != null &&
+                Boolean.parseBoolean(attributes.getNamedItem("pk").getNodeValue());
+        boolean nullable = attributes.getNamedItem("nullable") != null &&
+                Boolean.parseBoolean(attributes.getNamedItem("nullable").getNodeValue());
+        String defaultVal = attributes.getNamedItem("default") != null ?
+                attributes.getNamedItem("default").getNodeValue() : null;
+        String referredEnum = attributes.getNamedItem("referred-enum") != null ?
+                attributes.getNamedItem("referred-enum").getNodeValue() : null;
+
+        if (type == null) {
+            throw new ValidationException("Attribute 'type' for field '" + name + "' is missing.");
+        }
+
+        Field field = new Field();
+        field.setName(name);
+        field.setType(parseFieldType(type));
+        if (field.getType() == Enum.class) {
+            field.setReferredEnum(referredEnum);
+        }
+        field.setDefaultVal(defaultVal);
+        field.setNullable(nullable);
+        field.setLenght(length);
+        field.setPk(pk);
+
+        return field;
+    }
+
+    private Class parseFieldType(String type) {
+        switch (type) {
+            case "string":
+                return String.class;
+            case "number":
+                return BigDecimal.class;
+            case "long":
+                return Long.class;
+            case "double":
+                return Double.class;
+            case "float":
+                return Float.class;
+            case "boolean":
+                return Boolean.class;
+            case "enum":
+                return Enum.class;
+            default:
+                return null;
+        }
+    }
+
+    public List<Enum> extractEnumsAndItems() throws XPathExpressionException, ValidationException {
+        List<Enum> enums = new ArrayList<>();
+
+        extractEnums(enums);
+        extractItems(enums);
+        return enums;
+    }
+
+    private void extractEnums(List<Enum> enums) throws XPathExpressionException {
+        NodeList tablesNodes = (NodeList) xPath
+                .compile("jmodel/enum")
+                .evaluate(model, XPathConstants.NODESET);
+
+        for (int i = 0; i < tablesNodes.getLength(); i++) {
+            Enum theEnum = new Enum();
+            theEnum.setName(tablesNodes.item(i)
+                    .getAttributes()
+                    .getNamedItem("name")
+                    .getNodeValue());
+            enums.add(theEnum);
+        }
+    }
+
+    private void extractItems(List<Enum> enums) throws XPathExpressionException, ValidationException {
+        for (Enum theEnum : enums) {
+            NodeList items = (NodeList) xPath
+                    .compile(String.format("jmodel/enum[@name='%s']/item", theEnum.getName()))
+                    .evaluate(model, XPathConstants.NODESET);
+
+            for (int i = 0; i < items.getLength(); i++) {
+                String item = null;
+                if (items.item(i).getFirstChild().getNodeValue() != null) {
+                    item = items.item(i).getFirstChild().getNodeValue().trim();
+                }
+                if (item == null || item.isEmpty()) {
+                    throw new ValidationException("Enum " + theEnum.getName() + " cannot have empty items");
+                }
+
+                theEnum.addItem(item);
+            }
+        }
+    }
+}

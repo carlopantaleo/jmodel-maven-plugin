0d9c62b0f9ba7a0d605f92530d1fbd37af1855e6
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2018-04-09 15:06:03 +0200

    Testing composite ids

---
+++ jmodel-sample-project/src/main/java/com/carlopantaleo/jmodel/dao/AnotherTableDao.java
@@ -0,0 +1,15 @@
+package com.carlopantaleo.jmodel.dao;
+
+import com.jmodel.generated.AnotherTable;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AnotherTableDao extends BaseDao<AnotherTable> {
+    @Autowired
+    public AnotherTableDao(SessionFactory sessionFactory) {
+        super(sessionFactory);
+        setClazz(AnotherTable.class);
+    }
+}
---
+++ jmodel-sample-project/src/test/java/com/carlopantaleo/jmodel/dao/AnotherTableDaoTest.java
@@ -0,0 +1,40 @@
+package com.carlopantaleo.jmodel.dao;
+
+import com.jmodel.generated.AnotherTable;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Transactional
+public class AnotherTableDaoTest {
+    @Autowired
+    private AnotherTableDao anotherTableDao;
+
+    @Test
+    public void extendedDaoWorks() {
+        AnotherTable anotherTable = new AnotherTable();
+        anotherTable.setAnotherPkField1("a");
+        anotherTable.setAnotherPkField2("b");
+        anotherTable.setAnotherNonPkField("c");
+        anotherTableDao.insert(anotherTable);
+        assertEquals(1, anotherTableDao.findAll().size());
+
+        AnotherTable myKey = new AnotherTable();
+        myKey.setAnotherPkField1("a");
+        myKey.setAnotherPkField2("b");
+        myKey = anotherTableDao.findOne(myKey);
+        assertEquals("c", myKey.getAnotherNonPkField());
+
+        myKey.setAnotherPkField2("c");
+        myKey = anotherTableDao.findOne(myKey);
+        assertNull(myKey);
+    }
+}
